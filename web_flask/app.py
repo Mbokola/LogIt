6bcd8089e8a08a11b7ec24fd617ec0f3e626010d
Mbokola <tonypeaks@gmail.com>
2024-02-29 21:33:17 +0000

    auth implementation

--- web_flask/app.py
+++ web_flask/app.py
@@ -2,18 +2,28 @@
 """ Flask application
 """
 
-from flask import Flask, render_template, request, redirect, url_for
+from models.auth import Auth
+from flask import Flask, render_template, request, redirect, url_for, make_response
 from models.log import Log
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 logging = Log()
+AUTH = Auth()
 host = '0.0.0.0'
 port = 5001
 
 
-@app.route('/', methods=['GET', 'DELETE'], strict_slashes=False)
+# Logging app routes
+@app.route('/', strict_slashes=False)
 def logs():
+    """ Displays logs
+    """
+    return render_template('about.html')
+
+
+@app.route('/home', strict_slashes=False)
+def home():
     """ Displays logs
     """
     return render_template('index.html')
@@ -24,11 +34,14 @@ def create_log():
     """ Creates a log
     """
     if request.method == 'POST':
+        session_id = request.cookies.get("session_id")
+        record = AUTH.get_user_from_session_id(session_id)
         log_name = request.form['Log Name']
+        log_name_with_email = f"{log_name}_{record.email}"
         fields = request.form.getlist('text')
-        if log_name not in logging.all_logs():
-            logging.create_log(log_name, fields)
-        return redirect(url_for('logs'))
+        if log_name_with_email not in logging.all_logs():
+            logging.create_log(log_name_with_email, fields)
+        return redirect(url_for('home'))
     return render_template('createlog.html')
 
 
@@ -37,20 +50,32 @@ def all_logs():
     """ Retrives all logs
     """
     all_logs = logging.all_logs()
-    return all_logs
+    session_id = request.cookies.get("session_id")
+    record = AUTH.get_user_from_session_id(session_id)
+    if not record:
+        return redirect(url_for("login"))
+    record = AUTH.get_user_from_session_id(session_id)
+    for idx, log in enumerate(all_logs):
+        if log == "users":
+            del all_logs[idx]
+    user_logs = [log.split("_")[0] for log in all_logs if log.split("_")[1] == record.email]
+    return user_logs
 
 
 @app.route('/log/<log_name>', methods=['GET', 'POST'], strict_slashes=False)
 def log_fields(log_name):
     """ Retrieves log fields
     """
-    log_fields = logging.get_log_field(log_name)
+    session_id = request.cookies.get("session_id")
+    record = AUTH.get_user_from_session_id(session_id)
+    log_name_with_email = f"{log_name}_{record.email}"
+    log_fields = logging.get_log_field(log_name_with_email)
     del log_fields[0]
     if request.method == 'POST':
         data = {}
         for field in log_fields:
             data[field] = request.form[field]
-        logging.make_log_entry(log_name, data)
+        logging.make_log_entry(log_name_with_email, data)
 
     return render_template('log.html', log_fields=log_fields)
 
@@ -59,8 +84,12 @@ def log_fields(log_name):
 def view_logs(log_name):
     """ Retrieves logs of log
     """
-    fields = logging.get_log_field(log_name)
-    content = logging.retrieve_log(log_name)
+    session_id = request.cookies.get("session_id")
+    record = AUTH.get_user_from_session_id(session_id)
+    log_name_with_email = f"{log_name}_{record.email}"
+
+    fields = logging.get_log_field(log_name_with_email)
+    content = logging.retrieve_log(log_name_with_email)
     return render_template('show_logs.html', log_name=log_name, log_fields=fields, content=content)
 
 
@@ -68,8 +97,11 @@ def view_logs(log_name):
 def view_page(log_name, id):
     """ Expands a log
     """
-    fields = logging.get_log_field(log_name)[1:]
-    log = logging.get_log(log_name, {"id": int(id)})
+    session_id = request.cookies.get("session_id")
+    record = AUTH.get_user_from_session_id(session_id)
+    log_name_with_email = f"{log_name}_{record.email}"
+    fields = logging.get_log_field(log_name_with_email)[1:]
+    log = logging.get_log(log_name_with_email, {"id": int(id)})
     contents = list(log[0])[1:]
     zipped_data = list(zip(fields, contents))
 
@@ -80,7 +112,10 @@ def view_page(log_name, id):
 def del_log(log_name):
     """ Deletes a log
     """
-    logging.del_log(log_name)
+    session_id = request.cookies.get("session_id")
+    record = AUTH.get_user_from_session_id(session_id)
+    log_name_with_email = f"{log_name}_{record.email}"
+    logging.del_log(log_name_with_email)
     return redirect(url_for('logs'))
 
 
@@ -91,5 +126,88 @@ def about():
     return render_template("about.html")
 
 
+# Auth service routes
+@app.route("/users", methods=["GET", "POST"], strict_slashes=False)
+def users():
+    """ Handles registration of a user
+    """
+    if request.method == "POST":
+        try:
+            email = request.form.get("your_email")
+            password = request.form.get("password")
+            print(email)
+            print(password)
+            created_user = AUTH.register_user(email, password)
+
+            return render_template("login.html")
+
+        except ValueError:
+            return "email already registered"
+    return render_template("login.html")
+
+
+@app.route("/sessions", methods=["GET", "POST"], strict_slashes=False)
+def login():
+    """ Handles user login
+    """
+    if request.method == "POST":
+
+        email = request.form.get("your_email_1")
+        password = request.form.get("password_1")
+        result = AUTH.valid_login(email, password)
+
+        if result:
+
+            session_id = AUTH.create_session(email)
+            response = make_response(redirect(url_for("home")))
+            response.set_cookie("session_id", session_id)
+
+            return response
+
+    return render_template("login.html")
+
+
+@app.route('/sessions', methods=["DELETE"], strict_slashes=False)
+def logout():
+    """ Ends a session and logs out user
+    """
+    session_id = request.cookies.get("session_id")
+    record = AUTH.get_user_from_session_id(session_id)
+
+    if record:
+        user_id = record.id
+        AUTH.destroy_session(user_id)
+    return make_response("", 200)
+
+
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """ handles password reset
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return "Succsful"
+    except ValueError:
+        return "unsccesful"
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password():
+    """ Updates the users' password
+    """
+    print(request.form.items())
+    email = request.form.get("your_email")
+    new_password = request.form.get("new_password")
+    reset_token = request.form.get("reset_token")
+
+    try:
+        AUTH._db.find_user_by(reset_token=reset_token)
+        AUTH.update_password(reset_token, new_password)
+        return "succesful"
+    except (NoResultFound, ValueError):
+        return "unsccesful"
+
+
 if __name__ == '__main__':
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
--- web_flask/static/scripts/nav.js
+++ web_flask/static/scripts/nav.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('home-link').addEventListener('click', function(event) {
         event.preventDefault(); // Prevent the default action
-        window.location.href = '/'; // Redirect to Flask route
+        window.location.href = '/home'; // Redirect to Flask route
     });
 });
 
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// JavaScript to redirect when "Create Log" link is clicked
+// JavaScript to redirect when "about" link is clicked
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('about-link').addEventListener('click', function(event) {
         event.preventDefault(); // Prevent the default action
@@ -26,6 +26,56 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('logit').addEventListener('click', function(event) {
         event.preventDefault(); // Prevent the default action
-        window.location.href = '/'; // Redirect to Flask route
+        window.location.href = '/sessions'; // Redirect to Flask route
+    });
+});
+
+// JavaScript to redirect when "Logout" link is clicked
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('logout').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default action
+
+        // Send a DELETE request to the logout route
+        fetch('/sessions', {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if (response.ok) {
+                // If the request is successful, redirect to another page
+                window.location.href = '/sessions'; // Redirect to home page or any other page
+            } else {
+                // Handle error if necessary
+                console.error('Failed to logout:', response.statusText);
+            }
+        })
+        .catch(error => {
+            // Handle error if necessary
+            console.error('Error logging out:', error);
+        });
+    });
+});
+
+// JavaScript to redirect when "Submit" link is clicked
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('logout').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default action
+
+        // Send a DELETE request to the logout route
+        fetch('/create', {
+            method: 'POST'
+        })
+        .then(response => {
+            if (response.ok) {
+                // If the request is successful, redirect to another page
+                window.location.href = '/home'; // Redirect to home page or any other page
+            } else {
+                // Handle error if necessary
+                console.error('unsuccesful:', response.statusText);
+            }
+        })
+        .catch(error => {
+            // Handle error if necessary
+            console.error('Error:', error);
+        });
     });
 });
