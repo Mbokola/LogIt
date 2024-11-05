9161b74d0e13b0a4b74f81d8e8a49b6749c5f106
Mbokola <tonypeaks@gmail.com>
2024-11-05 15:01:04 +0300

    update connection string

--- models/db.py
+++ models/db.py
@@ -16,8 +16,8 @@ class DB:
         user = getenv("USER")
         password = getenv("PASSWORD")
         host = getenv("HOST")
-        self._engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:5432/logs?sslmode=require")
-        self._auth_engine = self._auth_engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:5432/auth?sslmode=require")
+        self._engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:5432/logs")
+        self._auth_engine = self._auth_engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:5432/auth")
         Base.metadata.create_all(self._auth_engine)
         self.metadata = MetaData()
         self.__session = None
